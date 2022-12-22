933ff69a1aff91ca883508cf97f9b27dddb9d4b9
Oleh Dubno <olehdubno@gmail.com>
2022-12-22 14:59:12 -0500

    add tries/total_number_of_words.py

--- tries/total_number_of_words.py
+++ tries/total_number_of_words.py
@@ -1,7 +1,7 @@
 from trie import Trie
 
 
-def total_number_of_words_in_a_trie(root: Trie, words=None):
+def total_number_of_words_in_a_trie(root: Trie):
     """
     1. we need to count all the instances of is_end_word
     2. we could use DFS to traverse all the nodes and
@@ -10,18 +10,19 @@ def total_number_of_words_in_a_trie(root: Trie, words=None):
     4. we will visit all the letters
     """
     if not root:
-        return len(words)
+        return None
+    # at each loop set word count at 0
+    word_count = 0
 
-    if words is None:
-        words = []
+    # increment the counter if the node is the end of the word
+    if root.is_end_word:
+        word_count += 1
 
     for curr_letter, curr_child in root.children.items():
-        if curr_child.is_end_word:
-            words.append(curr_letter)
-        total_number_of_words_in_a_trie(curr_child, words)
+        # continue to recursively increment the counter for each child
+        word_count += total_number_of_words_in_a_trie(curr_child)
 
-    return len(words)
-
+    return word_count
 
 
 def main():
